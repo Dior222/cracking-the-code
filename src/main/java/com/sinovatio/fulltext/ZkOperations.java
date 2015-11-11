2e15a8befc92dfb96e3f20d30293b3a770978adc
darwin <liufenglin@126.com>
2015-11-11 20:45:39 +0800

    why

--- src/main/java/com/sinovatio/fulltext/ZkOperations.java
+++ src/main/java/com/sinovatio/fulltext/ZkOperations.java
@@ -1,7 +1,11 @@
 package com.sinovatio.fulltext;
 
+import org.apache.zookeeper.ZooKeeper;
+
 public class ZkOperations {
 
+	private String connStr = "127.0.0.1:2181" ;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
